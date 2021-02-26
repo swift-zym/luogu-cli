25239911fcc3ada7a11b1e187ad91b4085c7967c
Yiming Zhang <i@zhangyiming.tech>
2021-02-26 23:38:26 +0800

    update problem

--- problem.js
+++ problem.js
@@ -4,18 +4,38 @@ const request = require('request-promise');
 module.exports = function (luogu) {
     luogu
         .command('problem <command> <params>')
-        .description('题目提交/查看 (send <file>|view <id>)')
+        .description('题目提交/查看 (send <file>|view <id>|search <keyword>)')
         .alias('p')
         .action(async function (command, file) {
             if (command == "view") {
                 try {
-                    let problem = await request({
+                    let json = await request({
                         url: config['luogu-domain'] + 'problem/' + file,
                         timeout: 1500,
                         headers: {
                             "x-luogu-type": "content-only"
-                        }
+                        },
+                        json:true
                     });
+                    let problem = json.currentData.problem;
+                    console.log(color.blue("题目背景:"));
+                    console.log(problem.background);
+                    console.log(color.blue("题目描述:"));
+                    console.log(problem.description);
+                    console.log(color.blue("输入格式:"));
+                    console.log(problem.inputFormat);
+                    console.log(color.blue("输出格式:"));
+                    console.log(problem.outputFormat);
+                    var idx=0;
+                    for(let sample of problem.samples){
+                        idx+=1;
+                        console.log(color.blue(`样例输入${idx}:`));
+                        console.log(sample[0]);
+                        console.log(color.blue(`样例输出${idx}:`));
+                        console.log(sample[1]);
+                    }
+                    console.log(color.blue("提示:"));
+                    console.log(problem.hint);
                 } catch (e) {
                     console.log(color.red('Error:'), "load problem fail.");
                 }
