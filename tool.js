dc69256c0904ac339b802e2dc7ff93e576ac3d23
swift-zym <zhangch1020@126.com>
2021-03-03 20:01:13 +0800

    update tool

--- tool.js
+++ tool.js
@@ -1,10 +1,25 @@
+const request = require("request-promise");
+const color = require("colors-cli");
+
 module.exports = function (luogu) {
     luogu
         .command('tool <command>')
-        .description('工具(captcha)')
+        .description('工具(captcha|check-online)')
         .action(async function (command){
             if(command == "captcha"){
                 await getCaptcha();
+            }
+            else if(command == "check-online"){
+                try{
+                    request({
+                        uri:config['luogu-domain'],
+                        timeout:1500
+                    });
+                    console.log(color.green("Luogu's server is online!"));
+                }
+                catch{
+                    console.log(color.red("Error: can't connet to luogu's server."));
+                }
             }
             else{
                 console.log("command not found,try --help.");
